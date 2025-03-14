6b71cc08a8678a5bb17c82d19a0a78a3b183ab5e
João Squinelato - Via Consulting <35571896+Squinelato@users.noreply.github.com>
2025-03-13 21:26:28 -0300

    feat: adding queue logic and automating deploy

--- lambdas/lambda-ingestion.py
+++ lambdas/lambda-ingestion.py
@@ -1,13 +1,12 @@
-from bs4 import BeautifulSoup
 from io import BytesIO
+from typing import NoReturn
 
 import os
 import requests
 import boto3
 
-def handler(event, context):
+def scrap_and_save_parquets(urls: list[str]) -> NoReturn:
 
-    url = os.environ['NYC_TLC_URL']
     bucket = os.environ['BUCKET_NAME']
     prefix = os.environ['BUCKET_PREFIX']
 
@@ -15,40 +14,42 @@ def handler(event, context):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
 
+    s3_client = boto3.client('s3')
+
+    for url in urls:
+        url_response = requests.get(url, headers=headers, stream=True)
+        url_response.raise_for_status()
+
+        parquet_name = url.split('/')[-1]
+        s3_key = prefix + parquet_name
+
+        with BytesIO(url_response.content) as file_buffer:
+
+            for chunk in url_response.iter_content(chunk_size=8192):
+                file_buffer.write(chunk)
+
+            file_buffer.seek(0)  # Reset buffer position to start
+
+            s3_client.upload_fileobj(file_buffer, bucket, s3_key)
+
+    # TODO: delete messages from queue
+
+def handler(event, context):
+
     try:
 
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
+        print('Start Lambda Ingestion')
 
-        soup = BeautifulSoup(response.text, features='html.parser')
+        print('Extracting the received list of URLs')
+        urls = set(map(lambda record: record['body'], event['Records']))
+        print('URLs: ', urls)
 
-        parquet_links: list = []
+        print('Scraping the parquet files and saving them on the raw layer')
+        scrap_and_save_parquets(urls)
 
-        for link in soup.find_all('a', href=True):
-            href = link['href']
-            if href.endswith('.parquet'):
-                parquet_links.append(href)
+        print('End Lambda Ingestion')
 
-        yellow_tripdata_links = \
-            list(filter(lambda href: 'yellow_tripdata' in href, parquet_links))
-        
-        s3_client = boto3.client('s3')
-
-        for link in yellow_tripdata_links:
-            link_response = requests.get(link, headers=headers, stream=True)
-            link_response.raise_for_status()
-
-            parquet_name = link.split('/')[-1]
-            s3_key = prefix + parquet_name
-
-            with BytesIO(link_response.content) as file_buffer:
-
-                for chunk in link_response.iter_content(chunk_size=8192):
-                    file_buffer.write(chunk)
-
-                file_buffer.seek(0)  # Reset buffer position to start
-
-                s3_client.upload_fileobj(file_buffer, bucket, s3_key)
+        return {'message': 'success'}
 
     except Exception as error:
         print("Error: ", error)
